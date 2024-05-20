245f9b883f3574255bcebe1204564905b1e38161
rbrenor2 <rbrenorios@gmail.com>
2024-05-20 18:08:07 +0100

    Initial Commit

---
+++ br2-ios-base/br2_ios_baseApp.swift
@@ -0,0 +1,17 @@
+//
+//  br2_ios_baseApp.swift
+//  br2-ios-base
+//
+//  Created by Breno R R on 20/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct br2_ios_baseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
